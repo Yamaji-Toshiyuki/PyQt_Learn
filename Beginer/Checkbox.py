e94226a0a138a7ba891ee2ca0841dbadad557f7e
toshiyuki@home <haruyuki.program@gmail.com>
2020-05-14 22:53:35 +0900

    グループ化

--- Beginer/Checkbox.py
+++ Beginer/Checkbox.py
@@ -7,7 +7,19 @@ class MainWindow(QWidget):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
-        self.test = QCheckBox('test', self)
+        # 1つ目のチェックボックス
+        self.upper = QCheckBox('大文字', self)
+        self.upper.move(100, 30)
+
+        # 2つ目のチェックボックス
+        self.lower = QCheckBox('小文字', self)
+        self.lower.move(180, 30)
+
+        # グループ化
+        self.group = QButtonGroup()
+        self.group.addButton(self.upper, 1)
+        self.group.addButton(self.lower, 2)
+
         self.setGeometry(300, 50, 400, 350)
         self.setWindowTitle('QCheckBox')
 
